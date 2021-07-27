824d559c0e4bbd3ea122f277b1a35321388d294e
Fomagran <fomagran6@naver.com>
2021-07-27 12:42:24 +0900

    Initial Commit

---
+++ SwiftUI Example/SwiftUI_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ExampleApp.swift
+//  SwiftUI Example
+//
+//  Created by Fomagran on 2021/07/27.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
