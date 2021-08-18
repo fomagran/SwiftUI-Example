b672a0099d2f03b103bd86298622481b0d169cc6
Fomagran <fomagran6@naver.com>
2021-08-18 12:48:26 +0900

    [add] 백그라운드 디자인하기

---
+++ Devote/View/BackgroundImageView.swift
@@ -0,0 +1,24 @@
+//
+//  BackgroundImageView.swift
+//  BackgroundImageView
+//
+//  Created by Fomagran on 2021/08/18.
+//
+
+import SwiftUI
+
+struct BackgroundImageView: View {
+    var body: some View {
+        Image("rocket")
+            .antialiased(true)
+            .resizable()
+            .scaledToFill()
+            .ignoresSafeArea(.all)
+    }
+}
+
+struct BackgroundImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        BackgroundImageView()
+    }
+}
--- Devote/View/ContentView.swift
+++ Devote/View/ContentView.swift
@@ -30,53 +30,65 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView {
-            VStack {
-                
-                VStack(spacing:16) {
-                    TextField("New Task",text: $task)
-                        .padding()
-                        .background(Color(UIColor.systemGray6))
-                        .cornerRadius(10)
-                    
-                    Button {
-                        addItem()
-                    } label: {
-                        Spacer()
-                        Text("SAVE")
-                        Spacer()
-                    }
-                    .disabled(isButtonDisabled)
-                    .padding()
-                    .font(.headline)
-                    .foregroundColor(.white)
-                    .background(isButtonDisabled ? Color.gray:Color.pink)
-                    .cornerRadius(10)
-                }
-                .padding()
-
-                List {
-                    ForEach(items) { item in
-                        VStack(alignment:.leading) {
-                            Text(item.task ?? "")
-                                .font(.headline)
-                                .fontWeight(.bold)
-                            Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                                .font(.footnote)
-                                .foregroundColor(.gray)
+            ZStack {
+                VStack {
+                    VStack(spacing:16) {
+                        TextField("New Task",text: $task)
+                            .padding()
+                            .background(Color(UIColor.systemGray6))
+                            .cornerRadius(10)
+                        
+                        Button {
+                            addItem()
+                        } label: {
+                            Spacer()
+                            Text("SAVE")
+                            Spacer()
                         }
+                        .disabled(isButtonDisabled)
+                        .padding()
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .background(isButtonDisabled ? Color.gray:Color.pink)
+                        .cornerRadius(10)
                     }
-                    .onDelete(perform: deleteItems)
-                }
-                .navigationBarTitle("Dailty Tasks",displayMode: .large)
-                .toolbar {
-#if os(iOS)
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        EditButton()
-                    }
-#endif
+                    .padding()
+
+                    List {
+                        ForEach(items) { item in
+                            VStack(alignment:.leading) {
+                                Text(item.task ?? "")
+                                    .font(.headline)
+                                    .fontWeight(.bold)
+                                Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+                                    .font(.footnote)
+                                    .foregroundColor(.gray)
+                            }
+                        }
+                        .onDelete(perform: deleteItems)
+                    }//List
+                    .listStyle(InsetGroupedListStyle())
+                    .shadow(color: Color(red: 0, green: 0, blue: 0,opacity: 0.3), radius: 12)
+                    .padding(.vertical,0)
+                    .frame(maxWidth:640)
                 }
             }
+            .onAppear(perform: {
+                UITableView.appearance().backgroundColor = UIColor.clear
+            })
+            .navigationBarTitle("Dailty Tasks",displayMode: .large)
+            .toolbar {
+                #if os(iOS)
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    EditButton()
+                }
+            #endif
+            }
+            .background(BackgroundImageView())
+            .background(backgroundGradient.ignoresSafeArea(.all))
+            .navigationViewStyle(StackNavigationViewStyle())
         }
+        
     }
     
     //MARK:- FUNCTION
