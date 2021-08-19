c12aafab02cf16e675fa73caa49f140b96bb7bd2
Fomagran <fomagran6@naver.com>
2021-08-19 11:21:15 +0900

    [add] New Task Button 디자인하고 누르면 NewTaskItemView 나오도록 구현

--- Devote/View/ContentView.swift
+++ Devote/View/ContentView.swift
@@ -13,9 +13,7 @@ struct ContentView: View {
     //MARK:- PROPERTY
     
     @State var task:String = ""
-    private var isButtonDisabled:Bool {
-        task.isEmpty
-    }
+    @State private var showNewTaskItem:Bool = false
     
     //FETCHING DATA
     @Environment(\.managedObjectContext) private var viewContext
@@ -31,29 +29,28 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             ZStack {
+          
                 VStack {
-                    VStack(spacing:16) {
-                        TextField("New Task",text: $task)
-                            .padding()
-                            .background(Color(UIColor.systemGray6))
-                            .cornerRadius(10)
-                        
-                        Button {
-                            addItem()
-                        } label: {
-                            Spacer()
-                            Text("SAVE")
-                            Spacer()
-                        }
-                        .disabled(isButtonDisabled)
-                        .padding()
-                        .font(.headline)
-                        .foregroundColor(.white)
-                        .background(isButtonDisabled ? Color.gray:Color.pink)
-                        .cornerRadius(10)
+                    
+                    Spacer(minLength: 80)
+                    
+                    Button {
+                        showNewTaskItem = true
+                    } label: {
+                        Image(systemName: "plus.circle")
+                            .font(.system(size: 30,weight: .semibold,design: .rounded))
+                        Text("New Task")
+                            .font(.system(size: 24,weight: .bold,design: .rounded))
                     }
-                    .padding()
+                    .foregroundColor(.white)
+                    .padding(.horizontal,20)
+                    .padding(.vertical,15)
+                    .background(LinearGradient(gradient: Gradient(colors: [Color.pink,Color.blue]), startPoint: .leading, endPoint: .trailing)
+                                    .clipShape(Capsule())
+                    )
+                    .shadow(color: Color(red: 0, green: 0, blue: 0,opacity: 0.3), radius: 8, x: 0, y: 4)
 
+                    
                     List {
                         ForEach(items) { item in
                             VStack(alignment:.leading) {
@@ -71,6 +68,17 @@ struct ContentView: View {
                     .shadow(color: Color(red: 0, green: 0, blue: 0,opacity: 0.3), radius: 12)
                     .padding(.vertical,0)
                     .frame(maxWidth:640)
+                }
+                
+                if showNewTaskItem {
+                    BlankView()
+                        .onTapGesture {
+                            withAnimation {
+                                showNewTaskItem = false
+                            }
+                        }
+                    
+                    NewTaskItemView(isShowing: $showNewTaskItem)
                 }
             }
             .onAppear(perform: {
@@ -92,26 +100,6 @@ struct ContentView: View {
     }
     
     //MARK:- FUNCTION
-    
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-            newItem.task = task
-            newItem.completion = false
-            newItem.id = UUID()
-            
-            do {
-                try viewContext.save()
-            } catch {
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-            
-            task = ""
-            hidKeyboard()
-        }
-    }
     
     private func deleteItems(offsets: IndexSet) {
         withAnimation {
