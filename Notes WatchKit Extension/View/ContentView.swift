ef877b632bc505569b2a3405a442c31ab75133bc
Fomagran <fomagran6@naver.com>
2021-08-30 11:15:23 +0900

    [add] 데이터 없을 때 디폴트 이미지 띄우기

--- Notes WatchKit Extension/View/ContentView.swift
+++ Notes WatchKit Extension/View/ContentView.swift
@@ -78,18 +78,29 @@ struct ContentView: View {
             }
             Spacer()
             
-            List {
-                ForEach(0..<notes.count,id:\.self) { i in
-                    HStack {
-                        Capsule()
-                            .frame(width: 4)
-                            .foregroundColor(.accentColor)
-                        Text(notes[i].text)
-                            .lineLimit(1)
-                            .padding(.leading,5)
-                    }//HStack
+            if notes.count >= 1 {
+                List {
+                    ForEach(0..<notes.count,id:\.self) { i in
+                        HStack {
+                            Capsule()
+                                .frame(width: 4)
+                                .foregroundColor(.accentColor)
+                            Text(notes[i].text)
+                                .lineLimit(1)
+                                .padding(.leading,5)
+                        }//HStack
+                    }
+                    .onDelete(perform: delete)
                 }
-                .onDelete(perform: delete)
+            }else {
+                Spacer()
+                Image(systemName: "note.text")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundColor(.gray)
+                    .opacity(0.25)
+                    .padding(24)
+                Spacer()
             }
         }
         .navigationTitle("Notes")
