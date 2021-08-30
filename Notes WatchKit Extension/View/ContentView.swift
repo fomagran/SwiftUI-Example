e12066de1da080985f467ddefc376718bd4a73d6
Fomagran <fomagran6@naver.com>
2021-08-30 11:23:06 +0900

    [add] 네비게이션 링크 달아서 해당 디테일뷰로 이동하게 하기

--- Notes WatchKit Extension/View/ContentView.swift
+++ Notes WatchKit Extension/View/ContentView.swift
@@ -81,13 +81,15 @@ struct ContentView: View {
             if notes.count >= 1 {
                 List {
                     ForEach(0..<notes.count,id:\.self) { i in
-                        HStack {
-                            Capsule()
-                                .frame(width: 4)
-                                .foregroundColor(.accentColor)
-                            Text(notes[i].text)
-                                .lineLimit(1)
-                                .padding(.leading,5)
+                        NavigationLink(destination: DetailView(note: notes[i], count: notes.count, index: i)) {
+                            HStack {
+                                Capsule()
+                                    .frame(width: 4)
+                                    .foregroundColor(.accentColor)
+                                Text(notes[i].text)
+                                    .lineLimit(1)
+                                    .padding(.leading,5)
+                            }
                         }//HStack
                     }
                     .onDelete(perform: delete)
