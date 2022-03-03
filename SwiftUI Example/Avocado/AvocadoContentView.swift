d6b98b8e2805215f62475445ba6f3c7fc8e3dc30
Fomagran <fomagran6@naver.com>
2022-03-02 21:45:58 -0600

    [Avocado] Asset 세팅

---
+++ SwiftUI Example/Avocado/AvocadoContentView.swift
@@ -0,0 +1,20 @@
+//
+//  AvocadoContentView.swift
+//  SwiftUI Example
+//
+//  Created by Fomagran on 2022/03/02.
+//
+
+import SwiftUI
+
+struct AvocadoContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AvocadoContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        AvocadoContentView()
+    }
+}
