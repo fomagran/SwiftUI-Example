cb5edbe72f7814170cc9686c0f8511eb568fa681
Fomagran <fomagran6@naver.com>
2022-03-02 22:16:28 -0600

    [Avocados] AvocadosView 레이아웃 및 디자인

--- SwiftUI Example/Avocado/View/AvocadosView.swift
+++ SwiftUI Example/Avocado/View/AvocadosView.swift
@@ -8,13 +8,62 @@
 import SwiftUI
 
 struct AvocadosView: View {
+    
+    //MARK:- Properties
+    
+    @State private var pulsateAnimation:Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+            Image("avocado")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 240, height: 240, alignment: .center)
+                .shadow(color: Color("ColorBlackTransparentDark"), radius: 12, x: 0, y: 8)
+                .scaleEffect(self.pulsateAnimation ? 1 : 0.9)
+                .opacity(self.pulsateAnimation ? 1 : 0.9)
+                .animation(Animation.easeOut(duration: 1.5).repeatForever(autoreverses: true))
+            
+            VStack {
+                Text("Avocados".uppercased())
+                    .font(.system(size:42,weight: .bold,design: .serif))
+                    .foregroundColor(Color.white)
+                    .padding()
+                .shadow(color: Color("ColorBlackTransparent"), radius: 4, x: 0, y: 4)
+                Text("""
+    Creamy, green, and full of nutrients!
+    Avocado is a powerhouse ingredient at any meal. Enjoy these handpicked avocado recipes
+    for breakfast, lunch, dinner & more!
+"""
+)
+                    .lineLimit(nil)
+                    .font(.system(.headline,design: .serif))
+                    .foregroundColor(Color("ColorGreenLight"))
+                    .multilineTextAlignment(.center)
+                    .lineSpacing(8)
+                    .frame(maxWidth: 640, minHeight:120)
+            }
+            .padding()
+            Spacer()
+        }
+        .background(
+        Image("background")
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+        )
+        .edgesIgnoringSafeArea(.all)
+        .onAppear {
+            self.pulsateAnimation.toggle()
+        }
+        
     }
 }
 
 struct AvocadosView_Previews: PreviewProvider {
     static var previews: some View {
         AvocadosView()
+            .previewDevice("iPhone 13 Pro")
+            .environment(\.colorScheme, .dark)
     }
 }
