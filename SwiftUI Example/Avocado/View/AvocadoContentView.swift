8ab4c7e294539aea2bb5008c5948a99efd4a2e2e
Fomagran <fomagran6@naver.com>
2022-03-21 21:56:23 -0500

    [Avocado] - HeaderView 레이아웃 및 애니메이션 적용 - ContenttView에 Footer 및 HeaderView 적용

--- SwiftUI Example/Avocado/View/AvocadoContentView.swift
+++ SwiftUI Example/Avocado/View/AvocadoContentView.swift
@@ -9,7 +9,35 @@ import SwiftUI
 
 struct AvocadoContentView: View {
     var body: some View {
-        Text("")
+        ScrollView(.vertical,showsIndicators: false) {
+            
+            //MARK:- Header
+            
+            ScrollView(.horizontal,showsIndicators: false) {
+                HStack(alignment:.top,spacing: 0) {
+                    HeaderView()
+                }
+            }
+            
+            //MARK:- Footer
+            
+            VStack(alignment: .center, spacing: 20) {
+                Text("All About Avocados")
+                    .font(.system(.title,design: .serif))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color("ColorGreenAdaptive"))
+                    .padding(8)
+                Text("Everything you wanted to know about avocados but were too afraid to ask")
+                    .font(.system(.body,design: .serif))
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(Color.gray)
+            }
+            .frame(maxWidth:640)
+            .padding()
+            .padding(.bottom,85)
+        }
+        .edgesIgnoringSafeArea(.all)
+        .padding(0)
     }
 }
 
